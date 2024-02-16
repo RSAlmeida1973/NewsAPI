b525f7420c4d262f04033a841e44a848c1f94fb1
Roberto Almeida <univil@gmail.com>
2024-02-15 18:03:03 -0600

    Added ability to select country and category.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const apiKey = process.env.NEWS_API_KEY;
-const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+let country = "us";
+let category = "general";
+let url = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=${country}&category=${category}`;
+
+document.querySelector("#btnUpdate").addEventListener("click", fetchNews);
 
 async function fetchNews() {
     try {
+      buildURL();
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
@@ -11,12 +16,28 @@ async function fetchNews() {
       console.error('There was an error!', error);
     }
   }
-  
-  fetchNews();
+
+  function buildURL() {
+    country = document.querySelector("#Country").value;
+    category = document.querySelector("#Category").value;
+    url = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=${country}&category=${category}`;
+    console.log(country);
+    console.log(category);
+    console.log(url);
+  }
 
   function displayNews(articles) {
     const newsDiv = document.querySelector('#news');
+
+    //Clear out the div.
+    while (newsDiv.firstChild) {
+      newsDiv.removeChild(newsDiv.firstChild);
+    }
+
+    //Populate the div.
     for (const article of articles) {
+      if (article.title !== "[Removed]") {
+
         const articleDiv = document.createElement('div');
 
         //create and append a headline to the articleDiv
@@ -24,8 +45,11 @@ async function fetchNews() {
         title.textContent = article.title;
         articleDiv.appendChild(title);
     
-      // TODO: Use document.createElement and appendChild to create and append more elements
-  
-      newsDiv.appendChild(articleDiv);
+        // TODO: Use document.createElement and appendChild to create and append more elements
+    
+        newsDiv.appendChild(articleDiv);
+      }
     }
 }
+  
+fetchNews();
